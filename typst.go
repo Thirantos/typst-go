f92a7f431f34b41ed56927323da96a8b28f8da65
Thijme van Son <Thijme@Thirantos.com>
2026-05-08 19:05:00 +0200

    added linux arm support

--- typst.go
+++ typst.go
@@ -5,7 +5,9 @@ package typst
 #cgo darwin,arm64 LDFLAGS: -L ./lib/aarch64-apple-darwin/ -ltypst -framework Security -framework CoreFoundation
 #cgo darwin,amd64 LDFLAGS: -L ./lib/x86_64-apple-darwin/ -ltypst -framework Security -framework CoreFoundation
 
+#cgo linux,arm64 LDFLAGS: -L ./lib/aarch64-unknown-linux-gnu/
 #cgo linux,amd64 LDFLAGS: -L ./lib/x86_64-unknown-linux-gnu/
+
 #cgo windows,amd64 LDFLAGS: -L ./lib/x86_64-pc-windows-msvc/
 
 #include <stdlib.h>
