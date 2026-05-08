6453c1d1c98ee535bdb27e2b342380d8b1692e5b
Thijme van Son <Thijme@Thirantos.com>
2026-05-08 19:42:03 +0200

    I suppose I also need libm don't I

--- typst.go
+++ typst.go
@@ -5,8 +5,8 @@ package typst
 #cgo darwin,arm64 LDFLAGS: -L ./lib/aarch64-apple-darwin/ -ltypst -framework Security -framework CoreFoundation
 #cgo darwin,amd64 LDFLAGS: -L ./lib/x86_64-apple-darwin/ -ltypst -framework Security -framework CoreFoundation
 
-#cgo linux,arm64 LDFLAGS: -L ./lib/aarch64-unknown-linux-gnu/ -ltypst
-#cgo linux,amd64 LDFLAGS: -L ./lib/x86_64-unknown-linux-gnu/ -ltypst
+#cgo linux,arm64 LDFLAGS: -L ./lib/aarch64-unknown-linux-gnu/ -ltypst -lm
+#cgo linux,amd64 LDFLAGS: -L ./lib/x86_64-unknown-linux-gnu/ -ltypst -lm
 
 #cgo windows,amd64 LDFLAGS: -L ./lib/x86_64-pc-windows-msvc/ -ltypst
 
